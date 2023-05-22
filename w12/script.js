40714aed62aa047292886e3a7150b14eaa58bd04
hajunsoo <angksehdfur2468@gmail.com>
2023-05-22 15:21:57 +0900

    d

---
+++ w12/script.js
@@ -0,0 +1,14 @@
+function add(left,right){
+    document.write(left+'+'+right+'=');
+    return left+ right;
+}
+function sub(left,right){
+    document.write(left+'-'+right+'=');
+    return left-right;
+}function mul(left,right){
+    document.write(left+'*'+right+'=');
+    return left*right;
+}function div(left,right){
+    document.write(left+'/'+right+'=');
+    return left/right;
+}
