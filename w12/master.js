066b771e569e7679ceb63fd6a33db6e6d41b27b4
hajunsoo <angksehdfur2468@gmail.com>
2023-05-22 15:33:41 +0900

    sdf

--- w12/master.js
+++ w12/master.js
@@ -4,12 +4,23 @@ var target = document.querySelector('body');
           target.style.backgroundColor = 'white';
           target.style.color = 'black';
           console.log(self);
-          self.value = 'night';
+          if(self.id == 'night_day_1'){
+            self.value = 'night';
           document.querySelector('#night_day_2').value = 'night';
+          }else{
+            self.value = 'night';
+          document.querySelector('#night_day_1').value = 'night';
+          }
+        
         } else {
           target.style.backgroundColor = 'black';
           target.style.color = 'white';
-          self.value = 'day';
+          if(self.id == 'night_day_1'){
+            self.value = 'day';
           document.querySelector('#night_day_2').value = 'day';
+          }else{
+            self.value = 'day';
+          document.querySelector('#night_day_1').value = 'day';
+          }
         }
     }
